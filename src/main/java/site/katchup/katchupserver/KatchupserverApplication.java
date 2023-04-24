9c7d2b9ed5dac381831af50bd3871572b12d8da9
unsualc@likelion.org <unusualc@likelion.org>
2023-04-24 17:30:20 +0900

    [CONFIG] initial setting

---
+++ src/main/java/site/katchup/katchupserver/KatchupserverApplication.java
@@ -0,0 +1,13 @@
+package site.katchup.katchupserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatchupserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatchupserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/katchup/katchupserver/KatchupserverApplicationTests.java
@@ -0,0 +1,13 @@
+package site.katchup.katchupserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatchupserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

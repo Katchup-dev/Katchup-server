0e5fd8a6c773e78cab15bd9bf5f613d79ba0195e
unsualc@likelion.org <unusualc@likelion.org>
2023-04-28 17:35:09 +0900

    [CONFIG] local security config class 작성

--- src/main/java/site/katchup/katchupserver/config/SecurityConfig.java
+++ src/main/java/site/katchup/katchupserver/config/SecurityConfig.java
@@ -1,2 +1,31 @@
-package site.katchup.katchupserver.config;public class SecurityConfig {
+package site.katchup.katchupserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    @Profile("local")
+    SecurityFilterChain localSecurityConfig(HttpSecurity http) throws Exception{
+        return http
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .cors()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                .anyRequest().permitAll()
+                .and()
+                .build();
+    }
 }

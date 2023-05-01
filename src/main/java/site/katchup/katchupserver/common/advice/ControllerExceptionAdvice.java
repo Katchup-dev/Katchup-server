07487bf902264f7ee8d55cc08a27f8748d883d77
yeseul106 <20191037@sungshin.ac.kr>
2023-05-02 01:14:04 +0900

    [CONFIG] ControllerAdvice 추가

---
+++ src/main/java/site/katchup/katchupserver/common/advice/ControllerExceptionAdvice.java
@@ -0,0 +1,22 @@
+package site.katchup.katchupserver.common.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import site.katchup.katchupserver.common.dto.ApiResponseDto;
+import site.katchup.katchupserver.common.exception.ErrorStatus;
+
+@RestControllerAdvice
+public class ControllerExceptionAdvice {
+
+    /*
+     * 400 BAD_REQUEST
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ApiResponseDto handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        return ApiResponseDto.error(ErrorStatus.VALIDATION_REQUEST_MISSING_EXCEPTION);
+    }
+}

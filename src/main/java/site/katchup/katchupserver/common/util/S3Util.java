eb55998808b8bb3e8547caa854c7adad44983e96
unsualc@likelion.org <unusualc@likelion.org>
2023-05-07 23:24:15 +0900

    [CHORE] 불필요한 import 삭제

--- src/main/java/site/katchup/katchupserver/common/util/S3Util.java
+++ src/main/java/site/katchup/katchupserver/common/util/S3Util.java
@@ -2,7 +2,6 @@ package site.katchup.katchupserver.common.util;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 

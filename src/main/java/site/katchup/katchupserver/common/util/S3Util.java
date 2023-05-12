222e35a4956bc6e11dbc62da6c1da2cdf8bc0026
unsualc@likelion.org <unusualc@likelion.org>
2023-05-12 10:48:12 +0900

    [FIX] S3Config class 선언 수정

--- src/main/java/site/katchup/katchupserver/common/util/S3Util.java
+++ src/main/java/site/katchup/katchupserver/common/util/S3Util.java
@@ -2,28 +2,32 @@ package site.katchup.katchupserver.common.util;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+import site.katchup.katchupserver.config.AwsS3Config;
 
 import java.net.URLDecoder;
 
-@Component
+@Configuration
 @RequiredArgsConstructor
 public class S3Util {
 
     private final Environment env;
-    private final AmazonS3Client amazonS3Client;
-    private final String bucket = env.getProperty("s3.bucket");
 
+    private final AwsS3Config awsS3Config;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
     // 파일 업로드
     public String upload(String fileName, String filePath) {
-        amazonS3Client.putObject(bucket, fileName, filePath);
-        return amazonS3Client.getUrl(bucket, fileName).toString();
+        awsS3Config.amazonS3Client().putObject(bucket, fileName, filePath);
+        return awsS3Config.amazonS3Client().getUrl(bucket, fileName).toString();
     }
 
     // 파일 삭제
     public void delete(String fileName) {
         String key = URLDecoder.decode(fileName.split("/")[3]);
-        amazonS3Client.deleteObject(bucket, key);
+        awsS3Config.amazonS3Client().deleteObject(bucket, key);
     }
 }

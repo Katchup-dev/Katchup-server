465742d1fad2849259e52e09e14bd587c77079cb
Seokyeong <purewater71@naver.com>
2023-05-16 23:04:51 +0900

    [Add] 유효하지 않은 토큰 검증 에러 핸들러 추가

---
+++ src/main/java/site/katchup/katchupserver/config/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,33 @@
+package site.katchup.katchupserver.config.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import site.katchup.katchupserver.common.dto.ApiResponseDto;
+import site.katchup.katchupserver.common.response.ErrorStatus;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        setResponse(response, ErrorStatus.UNAUTHORIZED_TOKEN);
+    }
+
+
+    private void setResponse(HttpServletResponse response, ErrorStatus status) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        ApiResponseDto<Object> apiResponse = ApiResponseDto.error(status);
+        response.getWriter().println(mapper.writeValueAsString(apiResponse));
+    }
+}

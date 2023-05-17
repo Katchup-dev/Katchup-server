a2364c9e4c1b0dcdd7e44206cfaaf3ba404403a2
yeseul106 <20191037@sungshin.ac.kr>
2023-05-18 02:57:01 +0900

    [CHORE] 명세서에 맞게 ApiResponseDto 수정

--- src/main/java/site/katchup/katchupserver/common/dto/ApiResponseDto.java
+++ src/main/java/site/katchup/katchupserver/common/dto/ApiResponseDto.java
@@ -12,20 +12,21 @@ import site.katchup.katchupserver.common.response.SuccessStatus;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApiResponseDto<T> {
 
-    private final int code;
+    private final int status;
+    private final boolean success;
     private final String message;
     private T data;
 
     public static ApiResponseDto success(SuccessStatus successStatus) {
-        return new ApiResponseDto<>(successStatus.getHttpStatus().value(), successStatus.getMessage());
+        return new ApiResponseDto<>(successStatus.getHttpStatus().value(), true, successStatus.getMessage());
     }
 
     public static <T> ApiResponseDto<T> success(SuccessStatus successStatus, T data) {
-        return new ApiResponseDto<T>(successStatus.getHttpStatus().value(), successStatus.getMessage(), data);
+        return new ApiResponseDto<T>(successStatus.getHttpStatus().value(), true, successStatus.getMessage(), data);
     }
 
     public static ApiResponseDto error(ErrorStatus errorStatus) {
-        return new ApiResponseDto<>(errorStatus.getHttpStatus().value(), errorStatus.getMessage());
+        return new ApiResponseDto<>(errorStatus.getHttpStatus().value(), false, errorStatus.getMessage());
     }
 }
 

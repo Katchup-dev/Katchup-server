8e7f997b396bdbcc37ec58df3999a084695561de
unsualc@likelion.org <unusualc@likelion.org>
2023-05-19 21:52:07 +0900

    [CHORE] response message 추가

--- src/main/java/site/katchup/katchupserver/common/response/ErrorStatus.java
+++ src/main/java/site/katchup/katchupserver/common/response/ErrorStatus.java
@@ -26,7 +26,8 @@ public enum ErrorStatus {
      */
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예상치 못한 서버 에러가 발생했습니다."),
     BAD_GATEWAY_EXCEPTION(HttpStatus.BAD_GATEWAY, "일시적인 에러가 발생하였습니다.\n잠시 후 다시 시도해주세요!"),
-    SERVICE_UNAVAILABLE_EXCEPTION(HttpStatus.SERVICE_UNAVAILABLE, "현재 점검 중입니다.\n잠시 후 다시 시도해주세요!");
+    SERVICE_UNAVAILABLE_EXCEPTION(HttpStatus.SERVICE_UNAVAILABLE, "현재 점검 중입니다.\n잠시 후 다시 시도해주세요!"),
+    IMAGE_UPLOAD_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드에 실패하였습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;

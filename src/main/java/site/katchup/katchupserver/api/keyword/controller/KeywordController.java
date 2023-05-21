0888e41bed18e40a7f2dc9087af1c7b184256d06
Seokyeong <purewater71@naver.com>
2023-05-21 22:14:26 +0900

    [CHORE] 키워드 목록 조회

--- src/main/java/site/katchup/katchupserver/api/keyword/controller/KeywordController.java
+++ src/main/java/site/katchup/katchupserver/api/keyword/controller/KeywordController.java
@@ -2,29 +2,22 @@ package site.katchup.katchupserver.api.keyword.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.katchup.katchupserver.api.keyword.dto.KeywordResponseDto;
 import site.katchup.katchupserver.api.keyword.service.KeywordService;
 import site.katchup.katchupserver.common.dto.ApiResponseDto;
 import site.katchup.katchupserver.common.response.SuccessStatus;
-import site.katchup.katchupserver.common.util.MemberUtil;
-
-import java.security.Principal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/keywords")
+@RequestMapping("/api/v1/cards")
 @RequiredArgsConstructor
 public class KeywordController {
     private final KeywordService keywordService;
 
-    @GetMapping()
+    @GetMapping("/{cardId}/keywords")
     @ResponseStatus(HttpStatus.OK)
-    public ApiResponseDto<List<KeywordResponseDto>> getAllKeyword(Principal principal) {
-        Long memberId = MemberUtil.getMemberId(principal);
-        return ApiResponseDto.success(SuccessStatus.GET_ALL_KEYWORD_SUCCESS, keywordService.getAllKeyword(memberId));
+    public ApiResponseDto<List<KeywordResponseDto>> getAllKeyword(@PathVariable Long cardId) {
+        return ApiResponseDto.success(SuccessStatus.GET_ALL_KEYWORD_SUCCESS, keywordService.getAllKeyword(cardId));
     }
 }

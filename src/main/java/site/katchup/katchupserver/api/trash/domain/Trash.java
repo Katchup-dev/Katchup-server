df3609fe366e21c1599264d5144ec670531c9fb8
unsualc@likelion.org <unusualc@likelion.org>
2023-08-12 00:04:18 +0900

    [CHORE] 사용하지 않는 import 제거

--- src/main/java/site/katchup/katchupserver/api/trash/domain/Trash.java
+++ src/main/java/site/katchup/katchupserver/api/trash/domain/Trash.java
@@ -5,13 +5,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import site.katchup.katchupserver.api.card.domain.Card;
-import site.katchup.katchupserver.api.folder.domain.Folder;
 import site.katchup.katchupserver.common.domain.BaseEntity;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import static jakarta.persistence.CascadeType.ALL;
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;

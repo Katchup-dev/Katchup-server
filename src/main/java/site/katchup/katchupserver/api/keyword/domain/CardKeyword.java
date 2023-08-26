43eaf18bbf647131bc82094c30ddeb814ba199f8
Seokyeong <purewater71@naver.com>
2023-08-26 16:33:38 +0900

    [DELETE] 불필요한 코드 삭제

--- src/main/java/site/katchup/katchupserver/api/keyword/domain/CardKeyword.java
+++ src/main/java/site/katchup/katchupserver/api/keyword/domain/CardKeyword.java
@@ -5,10 +5,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import site.katchup.katchupserver.api.card.domain.Card;
-import site.katchup.katchupserver.api.file.dto.request.FileCreateRequestDto;
-import site.katchup.katchupserver.api.screenshot.dto.request.ScreenshotCreateRequestDto;
-
-import java.util.List;
 
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.*;

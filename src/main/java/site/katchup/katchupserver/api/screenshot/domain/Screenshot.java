47416970b43926ea46c0e123c05a66da5fdcc806
Seokyeong <purewater71@naver.com>
2023-10-23 00:52:09 +0900

    [ADD] screenshot 테이블에 screenshotName 추가

--- src/main/java/site/katchup/katchupserver/api/screenshot/domain/Screenshot.java
+++ src/main/java/site/katchup/katchupserver/api/screenshot/domain/Screenshot.java
@@ -1,6 +1,8 @@
 package site.katchup.katchupserver.api.screenshot.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,6 +29,9 @@ public class Screenshot extends BaseEntity {
     @Column(nullable = false)
     private String url;
 
+    @Column(nullable = false, name = "스크린샷 이름.jpg")
+    private String screenshotName;
+
     @Column(nullable = false, name = "screenshot_key")
     private String screenshotKey;
 
@@ -41,9 +46,10 @@ public class Screenshot extends BaseEntity {
     private List<Sticker> sticker = new ArrayList<>();
 
     @Builder
-    public Screenshot(UUID id, String url, String screenshotKey, String uploadDate, Card card) {
+    public Screenshot(UUID id, String url, String screenshotName, String screenshotKey, String uploadDate, Card card) {
         this.id = id;
         this.url = url;
+        this.screenshotName = screenshotName;
         this.screenshotKey =screenshotKey;
         this.uploadDate = uploadDate;
         this.card = card;
